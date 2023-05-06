900d8dcce09062c701c41f33e96ec2e8bf35cb79
devvlopar <devangsingh34@gmail.com>
2023-05-06 11:34:05 +0530

    models

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.EmailField(unique = False)
+    password = models.CharField(max_length=150)
+    address = models.TextField()
+    pic = models.FileField(upload_to='profile_pics', default='sad.png')
+
+    def __str__(self):
+        return self.email
